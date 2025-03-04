9f371ee015ad71f4ab45ca5e6e0eba91ae2e9a06
owenlittlejohns <owenlittlejohns@gmail.com>
2025-03-04 11:10:49 -0700

    TRT-571 - Update net2cog to process multiple variables. (#38)
    Co-authored-by: Frank Greguska <89428916+frankinspace@users.noreply.github.com>

--- tests/test_netcdf_convert.py
+++ tests/test_netcdf_convert.py
@@ -5,95 +5,88 @@ test_netcdf_convert.py
 
 Test the netcdf conversion functionality.
 """
-import os.path
 import pathlib
 import subprocess
-from os import walk
-from os.path import dirname, join, realpath
+from os.path import basename, splitext
 
 import pytest
 
-from net2cog import netcdf_convert
-from net2cog.netcdf_convert import Net2CogError
+from net2cog.netcdf_convert import Net2CogError, netcdf_converter
 
 
-@pytest.fixture(scope='session')
-def data_dir():
-    test_dir = dirname(realpath(__file__))
-    test_data_dir = join(test_dir, 'data')
-    return test_data_dir
-
-
-@pytest.fixture(scope="function")
-def output_basedir(tmp_path):
-    return tmp_path
-
-
-@pytest.mark.parametrize('data_file', [
-    'SMAP_RSS_L3_SSS_SMI_8DAY-RUNNINGMEAN_V4/RSS_smap_SSS_L3_8day_running_2020_005_FNL_v04.0.nc'
-])
-def test_cog_generation(data_file, data_dir, output_basedir):
+def test_single_cog_generation(smap_file, temp_dir, logger):
     """
     Test that the conversion works and the output is a valid cloud optimized geotiff
     """
-    test_file = pathlib.Path(data_dir, data_file)
+    test_file = pathlib.Path(temp_dir, smap_file)
 
-    results = netcdf_convert.netcdf_converter(test_file, pathlib.Path(output_basedir, data_file), [])
+    results = netcdf_converter(
+        test_file,
+        pathlib.Path(temp_dir),
+        ['sss_smap'],
+        logger,
+    )
 
-    assert len(results) > 0
+    assert len(results) == 1
 
-    for entry in results:
-        if pathlib.Path(entry).is_file():
-            cogtif_val = [
-                'rio',
-                'cogeo',
-                'validate',
-                entry
-            ]
+    if pathlib.Path(results[0]).is_file():
+        assert basename(results[0]) == 'sss_smap.tif'
+        cogtif_val = [
+            'rio',
+            'cogeo',
+            'validate',
+            results[0]
+        ]
 
-            process = subprocess.run(cogtif_val, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-            cog_test = process.stdout
-            cog_test = cog_test.replace("\n", "")
+        process = subprocess.run(cogtif_val, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+        cog_test = process.stdout
+        cog_test = cog_test.replace("\n", "")
 
-            valid_cog = entry + " is a valid cloud optimized GeoTIFF"
-            assert cog_test == valid_cog
+        valid_cog = results[0] + " is a valid cloud optimized GeoTIFF"
+        assert cog_test == valid_cog
 
 
-@pytest.mark.parametrize(['data_file', 'in_bands'], [
-    ['SMAP_RSS_L3_SSS_SMI_8DAY-RUNNINGMEAN_V4/RSS_smap_SSS_L3_8day_running_2020_005_FNL_v04.0.nc', ['gland', 'fland', 'sss_smap']]
-])
-def test_band_selection(data_file, in_bands, data_dir, output_basedir):
+@pytest.mark.parametrize(['in_bands'], [[['gland', 'fland', 'sss_smap']]])
+def test_multiple_variable_selection(in_bands, temp_dir, smap_file, logger):
     """
     Verify the correct bands asked for by the user are being converted
     """
 
     in_bands = sorted(in_bands)
-    test_file = pathlib.Path(data_dir, data_file)
+    test_file = pathlib.Path(temp_dir, smap_file)
 
-    results = netcdf_convert.netcdf_converter(test_file, pathlib.Path(output_basedir, data_file), in_bands)
+    results = netcdf_converter(
+        test_file,
+        pathlib.Path(temp_dir),
+        in_bands,
+        logger
+    )
 
     assert len(results) == 3
 
     out_bands = []
     for entry in results:
         if pathlib.Path(entry).is_file():
-            band_completed = entry.split("4.0_")[-1].replace(".tif", "")
+            band_completed = splitext(basename(entry))[0]
             out_bands.append(band_completed)
 
     out_bands.sort()
     assert in_bands == out_bands
 
 
-@pytest.mark.parametrize(['data_file', 'in_bands'], [
-    ['SMAP_RSS_L3_SSS_SMI_8DAY-RUNNINGMEAN_V4/RSS_smap_SSS_L3_8day_running_2020_005_FNL_v04.0.nc', ['waldo']]
-])
-def test_unknown_band_selection(data_file, in_bands, data_dir, output_basedir):
+@pytest.mark.parametrize(['in_bands'], [[['waldo']]])
+def test_unknown_band_selection(in_bands, temp_dir, smap_file, logger):
     """
     Verify an incorrect band asked for by the user raises an exception
     """
 
     in_bands = sorted(in_bands)
-    test_file = pathlib.Path(data_dir, data_file)
+    test_file = pathlib.Path(temp_dir, smap_file)
 
     with pytest.raises(Net2CogError):
-        netcdf_convert.netcdf_converter(test_file, pathlib.Path(output_basedir, data_file), in_bands)
+        netcdf_converter(
+            test_file,
+            pathlib.Path(temp_dir),
+            in_bands,
+            logger
+        )
